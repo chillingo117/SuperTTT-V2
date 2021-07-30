77e775f8b0bc6c0e77256a297d81e37702698557
William <kiwitwwilliam@gmail.com>
2021-07-30 19:55:50 +1200

    bare bones of GameEnv and Main

---
+++ src/game/GameEnv.java
@@ -0,0 +1,21 @@
+package game;
+
+/**
+ * Contains the main function which initiates the game.
+ * A GameEnvironment is instantiated when the game starts.
+ *
+ */
+
+public class GameEnv {
+    private static GameEnv env = null;
+
+    private GameEnv () {
+    }
+
+    public getGame(){
+        if(env == null){
+            end = new GameEnv();
+        }
+    }
+
+}
